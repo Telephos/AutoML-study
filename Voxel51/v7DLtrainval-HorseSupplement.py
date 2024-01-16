03664dbed365cbbb97dd3da011adea4a4c8c4b18
Sam <deolivs@gmail.com>
2024-01-16 13:42:45 -0500

    Add files via upload

---
+++ Voxel51/v7DLtrainval-HorseSupplement.py
@@ -0,0 +1,24 @@
+import fiftyone as fo
+import fiftyone.zoo as foz
+from fiftyone import ViewField as F
+
+dataset = foz.load_zoo_dataset(
+    "open-images-v7",
+    splits=("train", "validation"),
+    label_types=["detections"],
+    classes=["Horse"],
+    drop_existing_dataset=False,
+    max_samples=50000,
+)
+
+dataset.persistent = True
+
+one_view = (
+    dataset
+    .select_fields("ground_truth")
+    .match(F("ground_truth.detections").length() == 1)
+)
+# dataset.save_view("one-view", one_view)
+
+session = fo.launch_app(one_view, desktop=True)
+session.wait()
---
+++ Voxel51/voc12DLtrainval.py
@@ -0,0 +1,24 @@
+import fiftyone as fo
+import fiftyone.zoo as foz
+from fiftyone import ViewField as F
+
+fo.config.default_ml_backend = "torch"
+
+dataset = foz.load_zoo_dataset(
+    "voc-2012",
+    splits=("train", "validation"),
+    label_types=["detections"],
+    drop_existing_dataset=False
+)
+
+dataset.persistent = True
+
+one_view = (
+    dataset
+    .select_fields("ground_truth")
+    .match(F("ground_truth.detections").length() == 1)
+)
+# dataset.save_view("one-view", one_view)
+
+session = fo.launch_app(one_view, desktop=True)
+session.wait()
